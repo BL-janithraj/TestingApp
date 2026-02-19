134eb784b1c494e8e0bd43950ff1506442decb3b
BL-janithraj <janithraj.jaysankar@bridgelabz.com>
2026-02-19 14:42:25 +0530

    frist_commit

---
+++ sample.js
@@ -0,0 +1,14 @@
+const name  =  "hello world";
+console.log(name);
+
+const age  = 20;
+function demo() {
+    const age = 30;
+    if(true) {
+        const age = 40;
+        console.log(age);
+    } 
+    console.log(age);
+}
+demo();
+console.log(age);
